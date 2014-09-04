3c64aebdb21065ba7b5fb64104885ad01358b0dd
Lee Boonstra <lee.boonstra@sencha.com>
2014-09-04 19:31:31 +0200

    minor changes

--- build/temp/development/FindACab/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Applications/XAMPP/xamppfiles/htdocs/hands-on-sencha-touch-2-examples/06-StructuredCode/1-Generating an Application with Sencha Cmd/app.js
--- build/temp/production/FindACab/sencha-compiler/app/full-page-master-bundle.js
+++
@@ -1,2 +0,0 @@
-// @tag full-page
-// @require /Applications/XAMPP/xamppfiles/htdocs/oreilly/1234000001571/include/mvc/generate-app/app.js
